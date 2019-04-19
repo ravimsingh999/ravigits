83ac4b1bd91273c28f057a0b65c24df61b7d2e5d
ravimsingh999 <49571521+ravimsingh999@users.noreply.github.com>
2019-04-19 23:09:42 +0530

    arranging the table according to burst time

--- osproject.c
+++ osproject.c
@@ -35,3 +35,58 @@ bt[i]=temp;
 }
 }
 }
+  /*Arranging the table according to Burst time,
+Execution time and Arrival Time
+Arrival time <= Execution time
+*/
+for(j=0;j<n;j++)
+{
+btime=btime+bt[j];
+min=bt[k];
+for(i=k;i<n;i++)
+{
+if (btime>=at[i] && bt[i]<min)
+{
+temp=p[k];
+p[k]=p[i];
+p[i]=temp;
+temp=at[k];
+at[k]=at[i];
+at[i]=temp;
+temp=bt[k];
+bt[k]=bt[i];
+bt[i]=temp;
+}
+}
+k++;
+}
+wt[0]=0;
+for(i=1;i<n;i++)
+{
+sum=sum+bt[i-1];
+wt[i]=sum-at[i];
+wsum=wsum+wt[i];
+}
+
+wavg=(wsum/n);
+for(i=0;i<n;i++)
+{
+ta=ta+bt[i];
+tt[i]=ta-at[i];
+tsum=tsum+tt[i];
+}
+
+tavg=(tsum/n);
+
+printf("************************");
+printf("\n RESULT:-");
+printf("\nProcess\t Burst\t Arrival\t Waiting\t Turn-around" );
+for(i=0;i<n;i++)
+{
+printf("\n p%d\t %d\t %d\t\t %d\t\t\t%d",p[i],bt[i],at[i],wt[i],tt[i]);
+}
+
+printf("\n\nAVERAGE WAITING TIME : %f",wavg);
+printf("\nAVERAGE TURN AROUND TIME : %f",tavg);
+return 0;
+}
